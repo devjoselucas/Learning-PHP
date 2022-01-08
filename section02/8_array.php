dd4e8ec4369b17403e73977781d0d21efc91c2e3
Dev Jose Lucas <jose.holanda@ee.ufcg.edu.br>
2022-01-08 11:38:39 -0300

    array

---
+++ section02/8_array.php
@@ -0,0 +1,13 @@
+<?php
+
+$arrayEmpty = [];
+$arrayList = [1,2,3,4,5,6,67];
+
+// echo $arrayEmpty;
+// echo $arrayList;
+
+print_r($arrayEmpty);
+echo "<br>";
+print_r($arrayList);
+echo "<br>";
+echo $arrayList[2];
