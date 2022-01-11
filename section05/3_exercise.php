9e4ca0bd92556fb92c549864e5cdb2848b1d199e
Dev Jose Lucas <jose.holanda@ee.ufcg.edu.br>
2022-01-11 14:22:25 -0300

    Conditionals

---
+++ section05/3_exercise.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = "a";
+$b = 10.5;
+$c = 10;
+
+if(is_string($a)){
+    echo "$a é string <br>";
+}
+
+if(is_float($b)) {
+    echo "$b é float <br>";
+}
+
+if(is_int($c)) {
+    echo "$c é inteiro <br>";
+}
+
+?>
