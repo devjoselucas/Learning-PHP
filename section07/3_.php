b2f1cbb71aa4fda49da0de28062d3b5e92692540
Dev Jose Lucas <jose.holanda@ee.ufcg.edu.br>
2022-01-19 08:25:00 -0300

    Form content

---
+++ section07/3_.php
@@ -0,0 +1,15 @@
+<?php
+
+$nome = "José";
+$sobrenome = "Lucas";
+
+?>
+
+<html>
+    <div>
+        <input type="text" value="<?= $nome ?>">
+    </div>
+    <div>
+        <input type="text" value="<?= $sobrenome ?>">
+    </div>
+</html>
